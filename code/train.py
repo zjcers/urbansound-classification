b4632491698145a3f7fc02c567bd00b497188911
Zane Cersovsky <zcersovsky@gmail.com>
2019-05-05 17:34:48 -0500

    Minor clean-up and added virtualenv settings

--- code/train.py
+++ code/train.py
@@ -20,22 +20,6 @@ from keras.layers import LSTM
 import params
 
 
-common.print_header("Loading Data")
-# labels, data = common.load_data()
-
-# print("Loaded", len(data),"items")
-
-# common.print_header("Calculating category vectors")
-# category_vecs = to_categorical(labels)
-
-# # Squish all of the MFCCs together into one numpy array
-# def add_dim(arr: numpy.ndarray):
-#     old_shape = arr.shape
-#     return arr.reshape(old_shape[0], old_shape[1], 1)
-# xs = numpy.stack(list(map(add_dim, data)), axis=0)
-# #xs = numpy.stack(data, axis=0)
-# print("Input data shape:", xs.shape)
-
 class DataGenerator(keras.utils.Sequence):
     """
     """
@@ -145,7 +129,7 @@ def cnn_model():
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.25))
     model.add(Flatten())
-    # model.add(Dense(10))
+    model.add(Dense(10))
     model.add(Dense(10))
     model.add(Activation("softmax"))
     model.compile(optimizer='adadelta',
