f370bc38b7e0c491e969532d2050d57fe1c11947
Zane Cersovsky <zcersovsky@gmail.com>
2019-05-05 22:59:28 -0500

    Rewrote preprocessing code to follow ETSI standard

--- code/common.py
+++ code/common.py
@@ -2,7 +2,7 @@ import csv
 import itertools
 import os
 import sys
-import concurrent.futures as concur
+import math
 import numpy
 import soundfile
 import librosa
@@ -10,29 +10,49 @@ import librosa.feature
 from keras.utils import to_categorical
 import params
 
+
 def ms_to_samples(sample_rate, ms):
     return (ms*sample_rate) // 1000
 
-def process_mfcc(y, sr, window_length = params.MFCC_WINDOW_LENGTH, n_mfcc = params.MFCC_BINS):
-    hop_length = ms_to_samples(sr, window_length)
-    mfcc = librosa.feature.mfcc(y=y,
-                                sr=sr,
+
+def process_mfcc(y, sr):
+    window_length = ms_to_samples(sr, params.MFCC_WINDOW_LENGTH)
+    hop_length = ms_to_samples(sr, params.MFCC_HOP_LENGTH)
+    S = librosa.core.stft(y,
+                          n_fft=window_length,
                                 hop_length=hop_length,
-                                n_mfcc=n_mfcc)
+                          center=False)
+    mfcc = librosa.feature.mfcc(S=S,
+                                fmin=params.MFCC_FMIN,
+                                hop_length=hop_length,
+                                n_mfcc=params.MFCC_BINS,
+                                n_fft=window_length)
     return mfcc
 
-def mfcc_reshape(mfcc: numpy.ndarray, depth=params.MFCC_CNN_DEPTH):
-    """
-    Reshapes MFCCs to be of fixed size
-    """
-    for lower in range(0, mfcc.shape[1], max(depth // params.MFCC_OVERLAP_FACTOR, 1)):
-        upper = lower + depth
-        if upper < mfcc.shape[1]:
-            # print("Slicing from", lower, "to", upper, "from", mfcc.shape)
-            yield mfcc[::, lower:upper]
 
-def load_wav_mono(filename):
-    y, sr = soundfile.read(filename)
+def compute_logE(y, sr):
+    window_length = ms_to_samples(sr, params.MFCC_WINDOW_LENGTH)
+    hop_length = ms_to_samples(sr, params.MFCC_HOP_LENGTH)
+    frames = librosa.util.frame(y,
+                                frame_length=window_length,
+                                hop_length=hop_length)
+    output = numpy.zeros(frames.shape[1])
+    # From ETSI ES 201 108
+    for frame in range(frames.shape[1]):
+        output[frame] = math.log(numpy.sum(frames[frame]**2))
+    return output
+
+
+def process_features(y, sr):
+    y, sr = to_mono(y, sr)
+    mfcc = process_mfcc(y, sr)
+    mfcc = mfcc.reshape((mfcc.shape[1], mfcc.shape[0]))
+    logE = compute_logE(y, sr)
+    logE = numpy.reshape(logE, (logE.shape[0], 1))
+    print(mfcc.shape, logE.shape)
+    return numpy.concatenate((mfcc, logE), axis=1)
+
+def to_mono(y, sr):
     if len(y.shape) == 2:
         y = y.reshape(2, y.shape[0])
         y = librosa.core.to_mono(y)
