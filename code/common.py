7042b0dd78524d69e49aefefe7826d495b64e318
Zane Cersovsky <zcersovsky@gmail.com>
2019-05-06 11:25:19 -0500

    Completely removed the caching mechanisms

--- code/common.py
+++ code/common.py
@@ -20,14 +20,14 @@ def process_mfcc(y, sr):
     hop_length = ms_to_samples(sr, params.MFCC_HOP_LENGTH)
     S = librosa.core.stft(y,
                           n_fft=window_length,
-                                hop_length=hop_length,
+                          hop_length=hop_length,
                           center=False)
     mfcc = librosa.feature.mfcc(S=S,
                                 fmin=params.MFCC_FMIN,
                                 hop_length=hop_length,
                                 n_mfcc=params.MFCC_BINS,
                                 n_fft=window_length)
-    return mfcc
+    return numpy.real(mfcc)
 
 
 def compute_logE(y, sr):
@@ -39,7 +39,14 @@ def compute_logE(y, sr):
     output = numpy.zeros(frames.shape[1])
     # From ETSI ES 201 108
     for frame in range(frames.shape[1]):
-        output[frame] = math.log(numpy.sum(frames[frame]**2))
+        try:
+            squared = numpy.real(frames[:, frame])**2
+            summed = numpy.sum(squared)
+            power = math.log(summed)
+            output[frame] = power
+        except ValueError:
+            # silence the math domain errors
+            output[frame] = 0.0
     return output
 
 
@@ -49,8 +56,17 @@ def process_features(y, sr):
     mfcc = mfcc.reshape((mfcc.shape[1], mfcc.shape[0]))
     logE = compute_logE(y, sr)
     logE = numpy.reshape(logE, (logE.shape[0], 1))
-    print(mfcc.shape, logE.shape)
     return numpy.concatenate((mfcc, logE), axis=1)
+
+def slice_inputs(mfcc: numpy.ndarray, depth=1):
+    """
+    Reshapes MFCCs to be of fixed size
+    """
+    for lower in range(0, mfcc.shape[0], depth):
+        upper = lower + depth
+        if upper < mfcc.shape[0]:
+            yield mfcc[lower:upper, :]
+
 
 def to_mono(y, sr):
     if len(y.shape) == 2:
@@ -58,133 +74,6 @@ def to_mono(y, sr):
         y = librosa.core.to_mono(y)
     return y, sr
 
-def find_mfcc(filename, y, sr, data_dir="tmp"):
-    try:
-        os.makedirs(os.path.join(data_dir, os.path.dirname(filename)))
-    except FileExistsError:
-        pass
-    sys.stdout.write("Getting MFCC for " + filename + "\r")
-    sys.stdout.flush()
-    full_name = os.path.join(data_dir, filename + "-" + str(params.MFCC_WINDOW_LENGTH) + "ms-"+str(params.MFCC_BINS)+"bins.npy")
-    try:
-        mfcc = numpy.load(full_name, mmap_mode='r')
-        return mfcc
-    except FileNotFoundError:
-        # y, sr = load_wav_mono(filename)
-        mfcc = process_mfcc(y, sr)
-        numpy.save(full_name, mfcc)
-        return numpy.load(full_name, mmap_mode='r') # don't keep the other mfcc around to keep memory usage in check
-        # return mfcc
-
 SLICE_FILE_NAME = 0
 FOLD = 5
 CLASS_ID = 6
-
-def augment(original_fn):
-    # try:
-    #     os.makedirs(os.path.join("tmp", os.path.dirname(original_path)))
-    # except FileExistsError:
-    #     pass
-    NOISE_SCALE = 0.001
-    SHIFT_SCALE = 0.1
-    both_channels, sr = soundfile.read(original_fn)
-    chans = range(1)
-    if len(both_channels.shape) == 2:
-        chans = range(both_channels.shape[1])
-        both_channels = both_channels.reshape(both_channels.shape[1], both_channels.shape[0])
-    else:
-        both_channels = both_channels.reshape(1, both_channels.shape[0])
-    for chan in chans:
-        original_data = both_channels[chan]
-        chan_lbl = "left" if chan == 0 else "right"
-        for shift in range(params.PITCH_LOWER, params.PITCH_UPPER + 1):
-            shift_scalar = SHIFT_SCALE * shift + 1.0
-            shifted_data = original_data
-            if shift != 0:
-                shifted_data = librosa.effects.pitch_shift(original_data, sr, shift_scalar)
-            for noise_level in range(0, params.NOISE_LEVELS + 1):
-                noise_scalar = NOISE_SCALE * noise_level
-                for pat in range(params.NOISE_PATTERNS + 1):
-                    name = os.path.join("tmp", "%s-shift-%s-noise-lvl%s-pat%s-%s" % (chan_lbl, shift_scalar, noise_scalar, pat, original_fn))
-                    # if not os.path.exists(name):
-                    noisy_data = shifted_data
-                    if noise_level != 0:
-                        noise = numpy.random.randn(len(original_data))
-                        noisy_data = original_data + noise_scalar * noise
-                    yield sr, shifted_data, name
-
-def process_file(fn, y, sr):
-    mfcc = find_mfcc(fn, y, sr)
-    for slice in mfcc_reshape(mfcc):
-        yield slice
-
-def process_row(row):
-    path = os.path.join("UrbanSound8K", "audio", "fold%s" % (row[FOLD],), row[SLICE_FILE_NAME])
-    slices = []
-    for sr, y, name in augment(path):
-        slices.extend(process_file(name, y, sr))
-    return zip(itertools.repeat(row[CLASS_ID]), slices)
-
-def load_data(label_file_name="UrbanSound8K/metadata/UrbanSound8K.csv", fold_selector=lambda _: True, augment=True):
-    with open(label_file_name, 'r') as labels:
-        reader = csv.reader(labels)
-        next(reader)
-        just_this_fold = filter(fold_selector, reader)
-        augmented_rows = []
-        if augment:
-            augmented_rows = itertools.chain.from_iterable(map(process_row, just_this_fold))
-        else:
-            for row in just_this_fold:
-                path = os.path.join("UrbanSound8K", "audio", "fold%s" % (row[FOLD],), row[SLICE_FILE_NAME])
-                y, sr = load_wav_mono(path)
-                for slice in mfcc_reshape(find_mfcc(path, y, sr)):
-                    augmented_rows.append((row[CLASS_ID], slice))
-        print()
-        cats = []
-        samples = []
-        for row in augmented_rows:
-            cats.append(row[0])
-            samples.append(row[1])
-        return samples, to_categorical(cats, 10)
-
-def print_header(text):
-    print(text.center(80, ' '))
-    print('=' * 80)
-
-def unique(iterable):
-    """
-    Filters iterable down to unique items while preserving order
-    """
-    seen = set()
-    for item in iterable:
-        if item not in seen:
-            seen.add(item)
-            yield item
-
-def add_dim(arr: numpy.ndarray):
-    old_shape = arr.shape
-    return arr.reshape(old_shape[0], old_shape[1], 1)
-
-def get_data(test_fold=1):
-    train_folds = [x for x in range(1, 3) if x != test_fold]
-    x_train = []
-    y_train = []
-    test_fold = str(test_fold)
-    x_test, y_test = load_data(fold_selector=lambda row: row[FOLD] == test_fold)
-    for fold in train_folds:
-        x_fold, y_fold = load_data(fold_selector=lambda row: row[FOLD] != test_fold)
-        x_train.extend(x_fold)
-        y_train.extend(y_fold)
-    x_train = numpy.stack(list(map(add_dim, x_train)), axis=0)
-    y_train = numpy.stack(y_train)
-    x_test = numpy.stack(list(map(add_dim, x_test)), axis=0)
-    numpy.save("tmp/x_train-%s.npy" % (test_fold,), x_train)
-    numpy.save("tmp/y_train-%s.npy" % (test_fold,), y_train)
-    numpy.save("tmp/x_test-%s.npy" % (test_fold,), x_test)
-    numpy.save("tmp/y_test-%s.npy" % (test_fold,), y_test)
-    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
-    x_train = numpy.load("tmp/x_train-%s.npy" % (test_fold,), mmap_mode='r')
-    y_train = numpy.load("tmp/y_train-%s.npy" % (test_fold,), mmap_mode='r')
-    x_test = numpy.load("tmp/x_test-%s.npy" % (test_fold,), mmap_mode='r')
-    y_test = numpy.load("tmp/y_test-%s.npy" % (test_fold,), mmap_mode='r')
-    return x_train, y_train, x_test, y_test
--- code/train.py
+++ code/train.py
@@ -27,105 +27,74 @@ class DataGenerator(keras.utils.Sequence):
         SLICE_FILE_NAME = 0
         FOLD = 5
         CLASS_ID = 6
-    def __init__(self, batch_size, test_fold, validation=False, path="UrbanSound8K", depth=params.MFCC_CNN_DEPTH):
+    def __init__(self, batch_size, test_fold, validation=False, path="UrbanSound8K", depth=1):
         self.batch_size = batch_size
         self.test_fold = test_fold
         self.validation = validation
         self.path = path
         self._depth = depth
-        self._length = 0
-        self._cache = []
-    def _list_wavs(self, for_train = True):
+        self.wavs = list(self._list_wavs())
+        self.outputshape = (params.BATCH_SIZE, params.MFCC_BINS+1, self._depth)
+        if depth > 1: # CNN
+            self.outputshape = self.outputshape + (1,)
+    def _list_wavs(self):
+        """
+        Reads the dataset CSV file and yields tuples of (path, fold, classid)
+        """
         with open(os.path.join(self.path, "metadata", "UrbanSound8K.csv"), 'r') as metadata:
             reader = csv.reader(metadata)
             next(reader) # skip header
             for row in reader:
-                folds_eq = row[self.Rows.FOLD] == self.test_fold
-                if (for_train and not folds_eq) or (not for_train and folds_eq):
-                    yield (row[self.Rows.SLICE_FILE_NAME], row[self.Rows.FOLD], int(row[self.Rows.CLASS_ID]))
-    def _get_all(self):
-        if self._length > 0:
-            return self._length
-        l = 0
-        for _ in self._sample_gen():
-            l += 1
-        self._cache = [None for _ in range(l)]
-        self._length = l
-        return l
+                fold = row[self.Rows.FOLD]
+                folds_eq = fold == self.test_fold
+                if (self.validation and folds_eq) or (not self.validation and not folds_eq):
+                    path = os.path.join(self.path, "audio", "fold%s" % (fold,), row[self.Rows.SLICE_FILE_NAME])
+                    yield (path, fold, int(row[self.Rows.CLASS_ID]))
     def __len__(self):
-        total_batches = self._get_all()
-        print("Will generate", total_batches)
-        return total_batches
-    def _sample_gen_slicer(self):
-        for wav in self._list_wavs():
-            for sr, y, name in common.augment(self._path_to(wav)):
-                mfcc = common.find_mfcc(name, y, sr)
-                for slice in common.mfcc_reshape(mfcc, self._depth):
-                    yield slice, wav[2]
-    def mkdir(self, filename):
-        try:
-            os.makedirs(os.path.dirname(filename))
-        except FileExistsError:
-            pass
-    def _sample_gen(self):
-        inputs = []
-        targets = []
-        amount_in_batch = 0
-        idx = 0
-        for slice, class_ in self._sample_gen_slicer():
-            path_mfcc, path_tags = self._path_to_batch(idx)
-            amount_in_batch += 1
-            if os.path.exists(path_mfcc):
-                if amount_in_batch == params.BATCH_SIZE:
-                    amount_in_batch = 0
-                    idx += 1
-                    yield
-            else:
-                inputs.append(slice)
-                targets.append(class_)
-                if amount_in_batch == params.BATCH_SIZE:
-                    cooked_inputs = numpy.stack(list(map(common.add_dim, inputs)), axis=0)
-                    cooked_targets = keras.utils.to_categorical(targets, 10)
-                    self.mkdir(path_mfcc)
-                    numpy.save(path_mfcc, cooked_inputs)
-                    numpy.save(path_tags, cooked_targets)
-                    yield
-                    idx += 1
-                    amount_in_batch = 0
-                    inputs.clear()
-                    targets.clear()
-    def _path_to(self, wav):
-        return os.path.join(self.path, "audio", "fold%s" % (wav[1],), wav[0])
-    def _path_to_batch(self, idx):
-        path_mfcc = "tmp/batch-size-%s-noise-%s-%s-pitch-%s-%s-bins-%s/%s-mfcc.npy" % (self.batch_size,
-                                                                                       params.NOISE_LEVELS,
-                                                                                       params.NOISE_PATTERNS,
-                                                                                       params.PITCH_LOWER,
-                                                                                       params.PITCH_UPPER,
-                                                                                       params.MFCC_BINS,
-                                                                                       idx)
-        path_tags = "tmp/batch-size-%s-noise-%s-%s-pitch-%s-%s-bins-%s/%s-tags.npy" % (self.batch_size,
-                                                                                       params.NOISE_LEVELS,
-                                                                                       params.NOISE_PATTERNS,
-                                                                                       params.PITCH_LOWER,
-                                                                                       params.PITCH_UPPER,
-                                                                                       params.MFCC_BINS,
-                                                                                       idx)
-        return path_mfcc, path_tags
+        slices = 0
+        for (path, _, _) in self.wavs:
+            with soundfile.SoundFile(path) as snd:
+                frame_length = common.ms_to_samples(snd.samplerate, params.MFCC_WINDOW_LENGTH)
+                hop_length = common.ms_to_samples(snd.samplerate, params.MFCC_HOP_LENGTH)
+                # From librosa.util.frame
+                n_frames = 1 + int((len(snd) - frame_length) / hop_length)
+                slices += n_frames
+        return slices // self.batch_size
+    def hash_int(self, n):
+        return (n * 2654435761) & 0xFFFFFFFF # 2^32 / Golden Ratio
+    def slice(self, wavIdx, thickness):
+        path, fold, class_ = self.wavs[wavIdx]
+        y, sr = soundfile.read(path)
+        inputs = common.process_features(y, sr)
+        c = 0
+        for slice_ in common.slice_inputs(inputs, thickness):
+            c += 1
+            yield slice_, class_
     def __getitem__(self, idx):
-        path_mfcc, path_tags = self._path_to_batch(idx)
-        self._get_all()
-        if self._cache[idx] is None:
-            self._cache[idx] = numpy.load(path_mfcc, mmap_mode='r'), numpy.load(path_tags, mmap_mode='r')
-        return self._cache[idx]
+        inputs = numpy.empty((params.BATCH_SIZE, self._depth, params.MFCC_BINS+1))
+        outputs = []
+        idx = self.hash_int(idx) % len(self.wavs)
+        outputIdx = 0
+        sourceIdxs = itertools.chain(range(idx, len(self.wavs)), \
+                     itertools.chain.from_iterable(
+                         itertools.repeat(
+                             range(0, len(self.wavs)))))
+        while outputIdx < params.BATCH_SIZE:
+            srcIdx = next(sourceIdxs)
+            for inputSlice,outputSlice in self.slice(srcIdx, self._depth):
+                inputs[outputIdx] = inputSlice
+                outputs.append(outputSlice)
+                outputIdx += 1
+                if outputIdx >= params.BATCH_SIZE:
+                    break
+        return numpy.reshape(inputs, self.outputshape), \
+               keras.utils.to_categorical(outputs, 10)
 def cnn_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(params.MFCC_BINS, params.MFCC_CNN_DEPTH,1), activation="relu"))
-    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(Conv2D(32, (3, 3), input_shape=(params.MFCC_BINS+1, params.MFCC_CNN_DEPTH, 1), activation="sigmoid"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.25))
-    model.add(Conv2D(16, (3, 3), activation="relu"))
-    model.add(Conv2D(16, (3, 3), activation="relu"))
+    model.add(Conv2D(16, (3, 3), activation="sigmoid"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.25))
     model.add(Flatten())
@@ -139,7 +108,7 @@ def cnn_model():
 
 def lstm_model():
     model = Sequential()
-    model.add(LSTM(params.MFCC_CNN_DEPTH, input_shape=(params.MFCC_BINS, 10)))
+    model.add(LSTM(params.MFCC_CNN_DEPTH, input_shape=(params.MFCC_BINS + 1, 1)))
     model.add(Dense(10))
     model.add(Activation("softmax"))
     model.compile(optimizer='adadelta',
@@ -152,13 +121,11 @@ def train_model(model, depth):
     earlystop = keras.callbacks.EarlyStopping(monitor="val_acc", min_delta=0.01, patience=12, restore_best_weights=True)
     tensorboard = keras.callbacks.TensorBoard(log_dir=("./logs-%s" % (sys.argv[1],)))
     train_gen = DataGenerator(params.BATCH_SIZE, "10", depth=depth)
-    x_test, y_test = common.load_data(fold_selector=lambda r: r[common.FOLD] == "10", augment=False)
-    x_test = numpy.stack(list(map(common.add_dim, x_test)), axis=0)
-    y_test = numpy.stack(y_test)
+    test_gen = DataGenerator(params.BATCH_SIZE, "10", validation=True, depth=depth)
     model.fit_generator(generator=train_gen,
-              validation_data=(x_test, y_test),
+              validation_data=test_gen,
               steps_per_epoch=len(train_gen),
-              use_multiprocessing=False,
+              use_multiprocessing=True,
               callbacks=[checkpoints, earlystop, tensorboard],
               epochs=200)
     return model
